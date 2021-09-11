17ff7394d67a461b9811887eb46cee677ae43dca
alig <475441@student.saxion.nl>
2021-09-11 20:32:19 +0200

    Header , Footer and navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap'
+import Header from './component/Header';
+import Footer from './component/Footer';
 const App = () => {
   return (
     <>
-      <h1>Welcome to pro shope</h1>
+      <Header/>
+      <main className='py-5'>
+        <Container>
+          <h1>Welcome to pro shope</h1>
+        </Container>
+      </main>
+      <Footer/>
     </>
   );
 }
