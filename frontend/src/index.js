3d36d62f7293ef7e5adc5bd7a3c82009013ba137
alig <475441@student.saxion.nl>
2021-09-18 18:52:39 +0200

    store implemented with redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
+
+import { Provider } from 'react-redux'
+// import * as serviceWorker from './serviceWorker'
+import store from './store.js'
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
